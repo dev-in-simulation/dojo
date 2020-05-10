08d74c1b76ee3ab3a30bf894000c594f24e71d40
Zach Niemann <czniemann1990@gmail.com>
2020-05-09 20:02:27 -0500

    Fix: updated manifest icon paths
    Mod: styling
    	- linking fonts instead of @import
    	- switched to the sass 7-1 pattern
    	- typography styles applied to basic tags

--- src/components/views/homepage.jsx
+++ src/components/views/homepage.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function Homepage() {
   return (
     <section id="homepage" className="view">
-      <h1 className="title">React Router Template</h1>
-      <span className="logo-container">
+      <h1 className="title">This is a TITLE</h1>
+      <h2 className="title">This is a TITLE</h2>
+      <h3 className="title">This is a TITLE</h3>
+      <h4 className="title">This is a TITLE</h4>
+      <h5 className="title">This is a TITLE</h5>
+      <h6 className="title">This is a TITLE</h6>
+      <span className="img-container custom" data-size="20px">
         <img
-          className="logo"
+          className="img"
           src="./assets/icons/beaker.svg"
           alt="default logo: a beaker made in svg"
         ></img>
