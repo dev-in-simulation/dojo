5ac3afb86b6b2a83bdde45ab9bbae24cd70fb7cf
Zach Niemann <czniemann1990@gmail.com>
2020-07-10 06:52:46 -0500

    Mod: <Footer /> 	- updated colors, styling and formatting 	- updated text

--- src/components/organisms/footer.jsx
+++ src/components/organisms/footer.jsx
@@ -7,10 +7,13 @@ function Footer(props) {
   return (
     <footer id="footer" className="page-view">
       <ViewSection>
-        <p className="closing-words content-2">{config.title}</p>
+        <p className="closing-words content-1 weight--bold">{config.title}</p>
+        <p className="closing-words content-4 weight--normal">
+          {config.subtitle}
+        </p>
 
         <DisplayIcons icons={config.iconsData} />
-        <p className="copyright content-4 weight--normal">{config.copyright}</p>
+        <p className="copyright content-5 weight--normal">{config.copyright}</p>
       </ViewSection>
     </footer>
   );
