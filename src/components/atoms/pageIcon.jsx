8b48649c7e190ab197608b9638271e966fc56da8
Zach Niemann <czniemann1990@gmail.com>
2020-07-14 18:25:40 -0500

    Add: <PageIcon> 	- description: displays an icon for each page, sitting at the 	  top center.
    	  - added skeleton component
    
    	  - corresponding sass file & added it
    	    to main.scss

---
+++ src/components/atoms/pageIcon.jsx
@@ -0,0 +1,13 @@
+//* used to display the icon that sits at the top
+//* of each page
+
+import React from "react";
+import PropTypes from "prop-types";
+
+function PageIcon(props) {
+  return <div className="page-icon">page icon placeholder</div>;
+}
+
+PageIcon.propTypes = {};
+
+export default PageIcon;
