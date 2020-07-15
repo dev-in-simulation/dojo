376bc81649822d94a4d2ec9f2e7bf2c845ff2130
Zach Niemann <czniemann1990@gmail.com>
2020-07-14 19:33:59 -0500

    Add: <PageIcon> 	- now accepts props with the path, alt-text, and 	  classes(optional) to use for the icon.
    	- page icons configured in pageConfig.js, passed it as 'data'
    	  prop

--- src/components/atoms/pageIcon.jsx
+++ src/components/atoms/pageIcon.jsx
@@ -4,14 +4,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function PageIcon(props) {
+function PageIcon({ data }) {
+  let iconClasses = data.addClasses
+    ? `page-icon ${data.addClasses}`
+    : "page-icon";
+
   return (
-    <div className="page-icon">
-      <img src="assets/icons/codeIcon.svg" alt="page icon"></img>
+    <div className={iconClasses}>
+      <img src={data.imgPath} alt={data.imgAlt}></img>
     </div>
   );
 }
 
-PageIcon.propTypes = {};
+PageIcon.propTypes = {
+  data: PropTypes.shape({
+    imgPath: PropTypes.string.isRequired,
+    imgAlt: PropTypes.string.isRequired,
+    addClasses: PropTypes.string,
+  }).isRequired,
+};
 
 export default PageIcon;
