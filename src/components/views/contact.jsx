48ef74852e6121aa254322afb7c76dfb67c3dbfd
Zach Niemann <czniemann1990@gmail.com>
2020-07-15 00:42:49 -0500

    added page icon to contact-page

--- src/components/views/contact.jsx
+++ src/components/views/contact.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import View from "../template/view";
 import ViewSection from "../template/viewSection";
 import ViewTitle from "../atoms/viewTitle";
+import PageIcon from "../atoms/pageIcon";
+import { ContactData } from "../helpers/pageConfig";
 
 function Contact(props) {
   return (
     <View id="contact">
+      <PageIcon data={ContactData.pageIcon} />
       <ViewTitle title="Say Hi" addClasses="page-title show-sides" />
     </View>
   );
