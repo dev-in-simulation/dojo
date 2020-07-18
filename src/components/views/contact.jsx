61aef0c97255e8834897bd9dfe0662e22ccad923
Zach Niemann <czniemann1990@gmail.com>
2020-07-18 00:39:59 -0500

    Add: <ContactForm> 	- basic functionality (minus post-request) done for contact form
    	- added scss file: _form.scss

--- src/components/views/contact.jsx
+++ src/components/views/contact.jsx
@@ -3,13 +3,16 @@ import View from "../template/view";
 import ViewSection from "../template/viewSection";
 import ViewTitle from "../atoms/viewTitle";
 import PageIcon from "../atoms/pageIcon";
+import ContactForm from "../molecules/contactForm";
 import { ContactConfig } from "../helpers/pageConfig";
 
 function Contact(props) {
   return (
     <View id="contact">
       <PageIcon data={ContactConfig.pageIcon} />
-      <ViewTitle title="Say Hi" addClasses="page-title show-sides" />
+      <ViewSection>
+        <ContactForm />
+      </ViewSection>
     </View>
   );
 }
