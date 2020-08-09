36bdbfea162aeea077db19bb5c617ea0defa6122
Zach Niemann <czniemann1990@gmail.com>
2020-08-09 17:40:11 -0500

    merge-fix3

--- src/components/views/about.jsx
+++ src/components/views/about.jsx
@@ -5,25 +5,11 @@ import ViewTitle from "../atoms/viewTitle";
 import View from "../template/view";
 import ViewSection from "../template/viewSection";
 import DisplayIcons from "../molecules/displayIcons";
-<<<<<<< HEAD
-=======
 import PageIcon from "../atoms/pageIcon";
->>>>>>> inbf-transfer-backup
 
 function About(props) {
   return (
     <View id="about">
-<<<<<<< HEAD
-      <ViewSection>
-        <ViewTitle title="About" addClasses="is-size-1 show-sides" />
-
-        <p className="subtitle is-size-1">{config.description}</p>
-        <p className="subtitle is-size-1">{config.description2}</p>
-      </ViewSection>
-
-      <ViewSection addClasses="skills">
-        <p className="title is-size-5">{config.dailyDescription}</p>
-=======
       <PageIcon data={config.pageIcon} />
       <ViewSection>
         <ViewTitle title="About" addClasses="page-title show-sides" />
@@ -34,18 +20,13 @@ function About(props) {
 
       <ViewSection addClasses="skills">
         <p className="content-2 weight--normal">{config.dailyDescription}</p>
->>>>>>> inbf-transfer-backup
         <DisplayIcons icons={config.iconsDaily} />
       </ViewSection>
 
       <ViewSection addClasses="skills">
-<<<<<<< HEAD
-        <p className="title is-size-5">{config.occasionalDescription}</p>
-=======
         <p className="content-2 weight--normal">
           {config.occasionalDescription}
         </p>
->>>>>>> inbf-transfer-backup
         <DisplayIcons icons={config.iconsOccasional} />
       </ViewSection>
     </View>
